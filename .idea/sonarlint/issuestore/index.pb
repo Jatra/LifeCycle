347548a3028d3f6c88c119868f4097992c41e032
Tim Russell <trussell@zipcar.co.uk>
2019-04-08 09:58:12 +0100

    Initial

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
+@
+app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
+_
+/app/src/main/java/uk/co/jatra/lifecycle/DA.java,f/8/f875dcfa0e762d9547bf0d7fc5e3e615d9e2c91f
+i
+9app/src/main/java/uk/co/jatra/lifecycle/MainActivity.java,d/6/d6dd763e58dc318b078137a985b3dcc544cc809f
+P
+ app/src/main/AndroidManifest.xml,8/c/8c55c3ccc257e5907959013f99656e4c8ec3903e
---
+++ app/src/main/java/uk/co/jatra/lifecycle/MainActivity.java
@@ -0,0 +1,106 @@
+package uk.co.jatra.lifecycle;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", this::showAlert).show();
+            }
+
+            private void showAlert(View view) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setMessage("ALERT!")
+                        .setPositiveButton("Dialog Activity", this::launchDialogActivity)
+                        .create()
+                        .show();
+            }
+
+            private void launchDialogActivity(DialogInterface dialogInterface, int i) {
+                Intent intent = new Intent(MainActivity.this, DA.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume() called");
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart() called");
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d(TAG, "onRestart() called");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onPostResume() {
+        Log.d(TAG, "onPostResume() called");
+        super.onPostResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause() called");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop() called");
+        super.onStop();
+    }
+}
